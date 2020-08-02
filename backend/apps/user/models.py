85f1ff792a63253b5cbdbc67769f72450d311b47
00marco <marcopulido00@gmail.com>
2020-08-02 17:31:14 +0800

    moved soft_delete implementation to soft_delete app

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -7,51 +7,6 @@ import datetime
 optional = {"blank": True, "null": True}
 
 
-class SoftDeletionQuerySet(QuerySet):
-    def delete(self):
-        return super(SoftDeletionQuerySet, self).update(deleted_at=datetime.datetime.now())
-
-    def hard_delete(self):
-        return super(SoftDeletionQuerySet, self).delete()
-
-    def alive(self):
-        return self.filter(deleted_at=None)
-
-    def dead(self):
-        return self.exclude(deleted_at=None)
-
-
-class SoftDeletionManager(models.Manager):
-    def __init__(self, *args, **kwargs):
-        self.alive_only = kwargs.pop('alive_only', True)
-        super(SoftDeletionManager, self).__init__(*args, **kwargs)
-
-    def get_queryset(self):
-        if self.alive_only:
-            return SoftDeletionQuerySet(self.model).filter(deleted_at=None)
-        return SoftDeletionQuerySet(self.model)
-
-    def hard_delete(self):
-        return self.get_queryset().hard_delete()
-
-
-class SoftDeletionModel(models.Model):
-    deleted_at = models.DateTimeField(blank=True, null=True)
-
-    objects = SoftDeletionManager()
-    all_objects = SoftDeletionManager(alive_only=False)
-
-    class Meta:
-        abstract = True
-
-    def delete(self):
-        self.deleted_at = datetime.datetime.now()
-        self.save()
-
-    def hard_delete(self):
-        super(SoftDeletionModel, self).delete()
-
-
 class Account(AbstractUser, SoftDeletionModel):
     username = models.CharField(blank=True, null=True, max_length=100)
     email = models.EmailField(("email address"), unique=True)
