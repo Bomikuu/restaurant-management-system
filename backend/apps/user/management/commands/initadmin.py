c4f47151dcd17464387c5fc9df3cbe786cf52d6d
Juan Alphonso D. Maligad <jdmaligad@gmail.com>
2020-08-23 22:53:35 +0800

    [CONFIG] Backend Docker

---
+++ backend/apps/user/management/commands/initadmin.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from .models import Account
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if Account.objects.count() == 0:
+            username = "admin@example.com"
+            email = "admin@example.com"
+            password = 'admin'
+            print('Creating account for %s (%s)' % (username, email))
+            admin = Account.objects.create_superuser(email=email, username=username, password=password)
+            admin.is_active = True
+            admin.is_admin = True
+            admin.save()
+        else:
+            print('Admin accounts can only be initialized if no Accounts exist')
