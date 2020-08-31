e086829b2709a3f44fd85fd90c546513ca294cf2
Mico Ang <mico@countable.ca>
2020-08-31 11:45:01 +0800

    [update] fixed media files not showing, and static files not showing in django admin

--- backend/conf/settings.py
+++ backend/conf/settings.py
@@ -138,11 +138,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = "/static/"
 STATIC_URL = "/static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-STATICFILES_DIRS = os.path.join(PROJECT_ROOT, 'static'),
 
 
 # LOCAL SETTINGS
