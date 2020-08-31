f1ab898bc3be93cbfd32bf352a99b0ba1d2b7f2f
Mico Ang <mico@countable.ca>
2020-08-31 11:45:22 +0800

    [update] refresh token every 5 mins

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,6 +11,45 @@ export default {
       JSON.parse(localStorage.getItem('currentUser'))
     )
     console.log(localStorage.getItem('currentUser'))
+  },
+  computed: {
+    getCurrentUser() {
+      return this.$store.state.currentUser
+    }
+  },
+
+  async mounted() {
+    if (this.getCurrentUser) {
+      if (this.getMinutes() > 5) {
+        //refresh token
+        this.refreshToken()
+      } else {
+        setInterval(() => {
+          if (this.getMinutes() > 5) {
+            this.refreshToken()
+          }
+        }, 5000)
+      }
+    }
+  },
+  methods: {
+    getMinutes() {
+      const { expire_data } = this.getCurrentUser
+
+      const dateNow = new Date()
+
+      const differenceInDate = dateNow - new Date(expire_data)
+      const diffMins = Math.round(
+        ((differenceInDate % 86400000) % 3600000) / 60000
+      )
+      return diffMins
+    },
+    async refreshToken() {
+      const result = await this.$store.dispatch('refreshToken')
+      if (result) {
+        console.log('REFRESHED TOKEN')
+      }
+    }
   }
 }
 </script>
