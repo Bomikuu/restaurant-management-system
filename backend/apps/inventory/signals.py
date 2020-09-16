e64f09438b30f30de5c6ee27b08859ebc6625791
00marco <marcopulido00@gmail.com>
2020-09-16 11:16:03 +0800

    feat - WIP. added post save signal to InventoryItem

---
+++ backend/apps/inventory/signals.py
@@ -0,0 +1,8 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import InventoryItem, InventoryItemLog
+
+
+@receiver(post_save, sender=InventoryItem)
+def create_new_log(sender, instance, **kwargs):
+    print("create inventory item log")
