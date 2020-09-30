dc6d8a3d628841f63f3991afe6d2ee286cd5ff45
Mico Ang <mico@countable.ca>
2020-09-30 16:54:28 +0800

    [update] added validation, and fixed formatting

--- frontend/src/views/allRoles.vue
+++ frontend/src/views/allRoles.vue
@@ -1,4 +1,3 @@
-
 <template>
   <v-container fluid class="pa-0">
     <div class="main-container">
@@ -9,163 +8,176 @@
               All Roles
             </div>
           </template>
-    
-     <v-toolbar flat color="white">
-      <v-spacer></v-spacer>
-      <v-dialog v-model="dialog" max-width="500px">
-        <template v-slot:activator="{ on }">
-          <v-btn color="primary" dark class="mb-2" v-on="on">New Role</v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="headline">{{ formTitle }}</span>
-          </v-card-title>
 
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editedItem.role" label="Role"></v-text-field>
-                </v-flex>
-                <v-flex xs12 sm6 md4>
-                  <v-text-field v-model="editedItem.permissions" label="Permissions"></v-text-field>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-text>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="Search"
+            single-line
+            hide-details
+          ></v-text-field>
 
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
-            <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-toolbar>
+          <v-data-table
+            :headers="headers"
+            :items="users"
+            :search="search"
+            :items-per-page="5"
+          >
+            <template v-slot:item.actions="{ item }">
+              <v-icon small class="mr-2" @click="editItem(item)">
+                mdi-pencil
+              </v-icon>
+              <v-icon small @click="deleteItem(item)">
+                mdi-delete
+              </v-icon>
+            </template>
 
-  <v-text-field
-              v-model="search"
-              append-icon="mdi-magnify"
-              label="Search"
-              single-line
-              hide-details
-            ></v-text-field>
-            
-    <v-data-table
-      :headers="headers"
-      :items="users"
-      :search="search"
-      :items-per-page="5"
-    >
-    
-     <template v-slot:item.actions="{ item }">
-      <v-icon
-        small
-        class="mr-2"
-        @click="editItem(item)"
-      >
-        mdi-pencil
-      </v-icon>
-      <v-icon
-        small
-        @click="deleteItem(item)"
-      >
-        mdi-delete
-      </v-icon>
-    </template>
-      
-    
-
-      <template v-slot:no-data>
-        <v-btn color="primary" @click="initialize">Reset</v-btn>
-      </template>
-      
-    </v-data-table>
+            <template v-slot:no-data>
+              <v-btn color="primary" @click="initialize">Reset</v-btn>
+            </template>
+          </v-data-table>
         </base-material-card>
       </v-col>
     </div>
+
+    <div class="my-2 add-product-btn" @click="toggleRoleModal">
+      <v-btn color="success" fab large dark>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </div>
+
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ formTitle }}</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-container grid-list-md>
+            <v-layout wrap>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  v-model="editedItem.role"
+                  label="Role"
+                ></v-text-field>
+              </v-flex>
+              <v-flex xs12 sm6 md4>
+                <v-text-field
+                  v-model="editedItem.permissions"
+                  label="Permissions"
+                ></v-text-field>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
 <script>
-  export default {
-    data: () => ({
-      dialog: false,
-       search: '',
-      headers: [
-        {
-          text: 'Role',
-          align: 'left',
-          value: 'role'
-        },
-        { text: 'Permissions', value: 'permissions' },
-         { text: 'Actions', value: 'actions', sortable: false }
-      ],
-      users: [],
-      editedIndex: -1,
-      editedItem: {
-        role: '',
-        permissions: '',
+export default {
+  data: () => ({
+    dialog: false,
+    search: '',
+    headers: [
+      {
+        text: 'Role',
+        align: 'left',
+        value: 'role'
       },
-      defaultItem: {
+      { text: 'Permissions', value: 'permissions' },
+      { text: 'Actions', value: 'actions', sortable: false }
+    ],
+    users: [],
+    editedIndex: -1,
+    editedItem: {
       role: '',
-        permissions: '',
-      }
-    }),
-
-    computed: {
-      formTitle () {
-        return this.editedIndex === -1 ? 'New Users' : 'Edit User'
-      }
+      permissions: ''
     },
+    defaultItem: {
+      role: '',
+      permissions: ''
+    }
+  }),
 
-    watch: {
-      dialog (val) {
-        val || this.close()
-      }
+  computed: {
+    formTitle() {
+      return this.editedIndex === -1 ? 'New Users' : 'Edit User'
+    }
+  },
+
+  watch: {
+    dialog(val) {
+      val || this.close()
+    }
+  },
+
+  created() {
+    this.initialize()
+  },
+
+  methods: {
+    toggleRoleModal() {
+      this.dialog = !this.dialog
     },
-
-    created () {
-      this.initialize()
-    },
-
-    methods: {
-      initialize () {
-        this.users = [
-          {
-            role: 'Admin',
-            permissions: 'Create,Update'
-          },
-        ]
-      },
-
-      editItem (item) {
-        this.editedIndex = this.users.indexOf(item)
-        this.editedItem = Object.assign({}, item)
-        this.dialog = true
-      },
-
-      deleteItem (item) {
-        const index = this.users.indexOf(item)
-        confirm('Are you sure you want to delete this item?') && this.users.splice(index, 1)
-      },
-
-      close () {
-        this.dialog = false
-        setTimeout(() => {
-          this.editedItem = Object.assign({}, this.defaultItem)
-          this.editedIndex = -1
-        }, 300)
-      },
-
-      save () {
-        if (this.editedIndex > -1) {
-          Object.assign(this.users[this.editedIndex], this.editedItem)
-        } else {
-          this.users.push(this.editedItem)
+    initialize() {
+      this.users = [
+        {
+          role: 'Admin',
+          permissions: 'Create,Update'
         }
-        this.close()
+      ]
+    },
+
+    editItem(item) {
+      this.editedIndex = this.users.indexOf(item)
+      this.editedItem = Object.assign({}, item)
+      this.dialog = true
+    },
+
+    deleteItem(item) {
+      const index = this.users.indexOf(item)
+      confirm('Are you sure you want to delete this item?') &&
+        this.users.splice(index, 1)
+    },
+
+    close() {
+      this.dialog = false
+      setTimeout(() => {
+        this.editedItem = Object.assign({}, this.defaultItem)
+        this.editedIndex = -1
+      }, 300)
+    },
+
+    save() {
+      if (this.editedIndex > -1) {
+        Object.assign(this.users[this.editedIndex], this.editedItem)
+      } else {
+        this.users.push(this.editedItem)
       }
+      this.close()
     }
   }
+}
 </script>
+
+<style lang="scss">
+.add-product-btn {
+  position: fixed;
+  z-index: 1000;
+  bottom: 2.5%;
+  right: 2.5%;
+
+  img {
+    width: 30px;
+    height: 30px;
+  }
+}
+</style>
--- frontend/src/views/allUsers.vue
+++ frontend/src/views/allUsers.vue
@@ -8,73 +8,6 @@
               All Users
             </div>
           </template>
-
-          <v-toolbar flat color="white">
-            <v-spacer></v-spacer>
-            <v-dialog v-model="dialog" max-width="500px">
-              <template v-slot:activator="{ on }">
-                <v-btn color="primary" dark class="mb-2" v-on="on"
-                  >New User</v-btn
-                >
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">{{ formTitle }}</span>
-                </v-card-title>
-
-                <v-card-text>
-                  <v-container grid-list-md>
-                    <v-layout wrap>
-                      <v-flex xs12 sm6 md6>
-                        <v-text-field
-                          v-model="editedItem.first_name"
-                          label="Firstname"
-                        ></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm6 md6>
-                        <v-text-field
-                          v-model="editedItem.last_name"
-                          label="Lastname"
-                        ></v-text-field>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                  <v-col cols="12">
-                    <v-text-field
-                      label="Email"
-                      v-model="editedItem.email"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-text-field
-                      type="password"
-                      label="Password"
-                      hint="At least 8 characters"
-                      v-model="editedItem.password"
-                      class="input-group--focused"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="6">
-                    <v-select
-                      v-model="editedItem.role"
-                      :items="this.roles"
-                      label="Role"
-                      required
-                    ></v-select>
-                  </v-col>
-                </v-card-text>
-
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" flat @click="close"
-                    >Cancel</v-btn
-                  >
-                  <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-toolbar>
-
           <v-text-field
             v-model="search"
             append-icon="mdi-magnify"
@@ -105,6 +38,75 @@
         </base-material-card>
       </v-col>
     </div>
+
+    <div class="my-2 add-product-btn" @click="handleAddUser">
+      <v-btn color="success" fab large dark>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </div>
+
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">{{ formTitle }}</span>
+        </v-card-title>
+
+        <v-card-text>
+          <v-form ref="form">
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm6 md6>
+                  <v-text-field
+                    v-model="editedItem.first_name"
+                    label="Firstname"
+                    :rules="rules.firstName"
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm6 md6>
+                  <v-text-field
+                    v-model="editedItem.last_name"
+                    label="Lastname"
+                    :rules="rules.lastName"
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+            <v-col cols="12">
+              <v-text-field
+                label="Email"
+                v-model="editedItem.email"
+                :rules="rules.email"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <v-text-field
+                type="password"
+                label="Password"
+                hint="At least 8 characters"
+                v-model="editedItem.password"
+                class="input-group--focused"
+                :rules="rules.password"
+              ></v-text-field>
+            </v-col>
+            <v-col cols="6">
+              <v-select
+                v-model="editedItem.role"
+                :items="this.roles"
+                label="Role"
+                :rules="rules.role"
+                required
+              ></v-select>
+            </v-col>
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" flat @click="close">Cancel</v-btn>
+          <v-btn color="blue darken-1" flat @click="save">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -144,6 +146,22 @@ export default {
       email: '',
       password: '',
       role: ''
+    },
+    rules: {
+      firstName: [v => !!v || 'First Name is required'],
+      lastName: [v => !!v || 'Last Name is required'],
+      email: [
+        v => !!v || 'Email is required',
+        v =>
+          !v ||
+          /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+          'E-mail must be valid'
+      ],
+      role: [v => !!v || 'Role is required'],
+      password: [
+        v => !!v || 'Password is required',
+        v => v.length > 7 || 'Password must be at least 6 characters'
+      ]
     }
   }),
 
@@ -172,6 +190,9 @@ export default {
     })
   },
   methods: {
+    handleAddUser() {
+      this.dialog = !this.dialog
+    },
     initialize() {
       this.roles = ['Admin']
     },
@@ -197,37 +218,55 @@ export default {
     },
 
     save() {
-      if (this.editedIndex > -1) {
-        API.patchAPI(
-          this.editedItem.url,
-          {
-            email: this.editedItem.email,
-            first_name: this.editedItem.first_name,
-            last_name: this.editedItem.last_name,
-            password: this.editedItem.password
-          },
-          this.token
-        ).then(response => {
-          console.log(response)
-        })
-        Object.assign(this.users[this.editedIndex], this.editedItem)
-      } else {
-        this.users.push(this.editedItem)
-        API.postAPI(
-          'users/',
-          {
-            email: this.editedItem.email,
-            first_name: this.editedItem.first_name,
-            last_name: this.editedItem.last_name,
-            password: this.editedItem.password
-          },
-          this.token
-        ).then(response => {
-          console.log(response)
-        })
+      const isValid = this.$refs.form.validate()
+
+      if (isValid) {
+        if (this.editedIndex > -1) {
+          API.patchAPI(
+            this.editedItem.url,
+            {
+              email: this.editedItem.email,
+              first_name: this.editedItem.first_name,
+              last_name: this.editedItem.last_name,
+              password: this.editedItem.password
+            },
+            this.token
+          ).then(response => {
+            console.log(response)
+          })
+          Object.assign(this.users[this.editedIndex], this.editedItem)
+        } else {
+          this.users.push(this.editedItem)
+          API.postAPI(
+            'users/',
+            {
+              email: this.editedItem.email,
+              first_name: this.editedItem.first_name,
+              last_name: this.editedItem.last_name,
+              password: this.editedItem.password
+            },
+            this.token
+          ).then(response => {
+            console.log(response)
+          })
+        }
+        this.close()
       }
-      this.close()
     }
   }
 }
 </script>
+
+<style lang="scss">
+.add-product-btn {
+  position: fixed;
+  z-index: 1000;
+  bottom: 2.5%;
+  right: 2.5%;
+
+  img {
+    width: 30px;
+    height: 30px;
+  }
+}
+</style>
